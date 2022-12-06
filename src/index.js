9e4fc63aeedbcc3b938335436216184e147ec939
shivprajapat <shivkumarprajapat232@gmail.com>
2022-12-06 18:33:55 +0530

    react icons global icon size add  using IconContext Provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { IconContext } from "react-icons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <IconContext.Provider value={{size:"30"}}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </IconContext.Provider>
   </React.StrictMode>
 );
 reportWebVitals();
