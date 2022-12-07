16c30cb73617062d1935fbafa327500f1a5c7468
shivprajapat <shivkumarprajapat232@gmail.com>
2022-12-07 12:19:05 +0530

    home page responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import { routerData } from "./components/routerData";
-import { Navbar } from "./components";
+import { Navbar, Theme } from "./components";
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <Navbar />
-          <Routes>
-        {
-          routerData && routerData.map((item, index) => {
-            const { path, pageName } = item;
-            return (
-              <Route key={index} path={path} element={pageName} />
-            )
-          })
-        }
-      </Routes>
-    </div>
+      <Theme />
+
+      <div className="App_main_wrapper">
+        <Routes>
+          {routerData &&
+            routerData.map((item, index) => {
+              const { path, pageName } = item;
+              return <Route key={index} path={path} element={pageName} />;
+            })}
+        </Routes>
+      </div>
+    </main>
   );
 }
 
